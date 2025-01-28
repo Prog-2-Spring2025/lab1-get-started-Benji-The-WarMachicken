fd591093aeb02e4b028481be59bd7d3b8bb938a7
Benji Paden <benji.paden@gmail.com>
2025-01-28 16:24:48 -0600

    Wait, it's all Ohio?

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
  */ 
 
 int main() {
-	cout << "Hello Programming II"; 
+	cout << "Star Trekkin' across the universe" << endl << "On the Starship Enterprise under Captain Kirk" << endl << "Star Trekkin' across the universe" << endl << "Boldly going forward 'cause we can't find reverse";
+		 
 	return 0; 
 }
