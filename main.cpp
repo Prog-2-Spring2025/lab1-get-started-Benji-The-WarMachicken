80959b658080a8ece286b42f181023bf46195d80
Benji Paden <benji.paden@gmail.com>
2025-01-28 16:27:29 -0600

    Always has been.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include "header.h"
 
-/*
- *  This is a blank template used when I don't have
- *  any specific pre-built code for you to start with
- *
- *  Rename the files in this template to make sense
- *  with the current assignment or lab exercise. 
- *
- * Then delete these comments, commit, and push your code.
- */ 
+
 
 int main() {
 	cout << "Star Trekkin' across the universe" << endl << "On the Starship Enterprise under Captain Kirk" << endl << "Star Trekkin' across the universe" << endl << "Boldly going forward 'cause we can't find reverse";
